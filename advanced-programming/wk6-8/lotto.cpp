ebea54deb6f580596c3b7693b230c0e0a3fb97e8
Livie <toni.sojakka@hotmail.com>
2020-02-12 09:49:37 +0200

    Add result to debug string wk6-8

--- advanced-programming/wk6-8/lotto.cpp
+++ advanced-programming/wk6-8/lotto.cpp
@@ -4,6 +4,7 @@
 
 LottoMachine::LottoMachine() {
     printf("LottoMachine constructor called\n");
+    result = LottoResult::NONE;
     correctRow = new int[7] {1, 2, 3, 4, 5, 6, 7};
 }
 
@@ -15,4 +16,18 @@ LottoMachine::~LottoMachine() {
 void LottoMachine::printDebugString() {
     printf("LottoMachine v0.3.14159 running\n");
     printf("Correct row is {%i, %i, %i, %i, %i, %i, %i}\n", *correctRow, *(correctRow + 1), *(correctRow + 2), *(correctRow + 3), *(correctRow + 4), *(correctRow + 5), *(correctRow + 6));
+    printf("Result is ");
+    switch (this->result) {
+        case LottoResult::NONE:
+            printf("NONE");
+            break;
+        case LottoResult::ERROR:
+            printf("ERROR");
+            break;
+        default:
+            printf("UNDEFINED");
+            break;
+    }
+    printf("\n");
+    fflush(stdin);
 }
