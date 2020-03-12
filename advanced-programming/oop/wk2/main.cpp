b24105186a203f9d98c38f527abc229a7a853b43
Livie <toni.sojakka@hotmail.com>
2020-03-12 13:53:05 +0200

    Add ctor/dtor tests to main

--- advanced-programming/oop/wk2/main.cpp
+++ advanced-programming/oop/wk2/main.cpp
@@ -4,12 +4,44 @@
 #include "person.h"
 #include "date.h"
 
+void testFunction() {
+    // Gets destroyed when function returns
+    Person jkl = Person("Jkl", 24);
+}
+
 int main() {
+
+    // Never gets destroyed unless delete is called
+    Person* mno = new Person("Mno", 25);
+
+
+
+    // Gets destroyed when program returns
     Person abc = Person("Abc", 21);
 
-    abc.printInfo();
+    std::cout << "Before if()\n";
+    if (true) {
+        // Gets destroyed when if-block ends
+        Person def = Person("Def", 22);
+    }
+    std::cout << "After if()\n";
 
-    Person people[10];
+    std::cout << "Before for()\n";
+    for (int i = 0; i < 10; i++) {
+        std::cout << "For loop i: " << i << '\n';
+        // Gets destroyed when for-block ends, every iteration
+        Person ghi = Person("Ghi", 23);
+        std::cout << "For loop end\n";
+    }
+    std::cout << "After for()\n";
 
+    std::cout << "Before function\n";
+    testFunction();
+    std::cout << "After function\n";
+
+    // Necessary for objects created with new
+    delete mno;
+
+    std::cout << "End of program" << std::endl;
     return 0;
 }
